751fdec39d0a13cc7b9e577868f243e9fe4cb947
Script Raccoon <54458975+ScriptRaccoon@users.noreply.github.com>
2021-10-20 12:00:32 +0200

    allow rotation of cube with keys

---
+++ main.js
@@ -0,0 +1,39 @@
+const STATUS = {
+    cubeRotationX: -45,
+    cubeRotationY: 45,
+    cubeRotationZ: 0,
+};
+
+const KEY_MAP = {
+    ArrowRight: () => (STATUS.cubeRotationY += 45),
+    ArrowLeft: () => (STATUS.cubeRotationY -= 45),
+    ArrowUp: () => (STATUS.cubeRotationX += 45),
+    ArrowDown: () => (STATUS.cubeRotationX -= 45),
+    4: () => (STATUS.cubeRotationZ -= 45),
+    6: () => (STATUS.cubeRotationZ += 45),
+};
+
+const ALLOWED_KEYS = Object.keys(KEY_MAP);
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    handleKeyDown();
+}
+
+function handleKeyDown() {
+    document.addEventListener("keydown", (e) => {
+        if (!ALLOWED_KEYS.includes(e.key)) return;
+        KEY_MAP[e.key]();
+        applyRotationToCube();
+    });
+}
+
+function applyRotationToCube() {
+    $("#cube").css(
+        "transform",
+        `rotateX(${STATUS.cubeRotationX}deg)
+        rotateY(${STATUS.cubeRotationY}deg)
+        rotateZ(${STATUS.cubeRotationZ}deg)`
+    );
+}
